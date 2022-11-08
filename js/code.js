cd9ee673857b6570c1f3810579927b9631a66c73
Hims47 <masizoleshabane57@gmail.com>
2022-11-08 10:06:57 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,8 @@
+// variables
+let userName = "juice"
+let myAge = 47;
+let booVal = false;
+document.write(userName);
+document.write("<br>"+myAge)
+document.write("<br>"+typeof booVal)
+let names = ['Daniel','Mishaine','Matthew'];
---
+++ js2/code2.js
@@ -0,0 +1,12 @@
+// write to a document 
+document.write("Hello World");
+document.write("<br>my name is See You Again");
+document.write("<br>Am a developer");
+/*
+you are
+awesome
+*/
+let userName = 'him';
+let userAge = 47;
+console.log(userName);
+console.log(typeof userName);
